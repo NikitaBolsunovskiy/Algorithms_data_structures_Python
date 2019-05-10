0f20b87c382c8ba73e241e4f2db0b7326892f1dd
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2019-05-10 10:45:11 +0300

    Lesson_7 HW1 end

--- Lesson_7/hw_1.py
+++ Lesson_7/hw_1.py
@@ -4,3 +4,59 @@
 функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
 
+import random
+
+
+def init_array(n):
+
+    a = []
+    for i in range(0, n):
+        a.append(random.randint(-100, 100))
+    return a
+
+
+def buble_sort(a):
+    counter = 0
+    m = 1
+    while m < len(a):
+        for i in range(len(a) - m):
+            counter += 1
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+        m += 1
+    print(f'Количество операций внутренних: {counter}')
+    return a
+
+
+def buble_sort_opt(a):
+    counter = 0
+    m = 1
+    was_moved = True
+    while m < len(a) and was_moved:
+        was_moved = False
+        for i in range(len(a) - m):
+            counter += 1
+            if a[i] > a[i + 1]:
+                was_moved = True
+                a[i], a[i + 1] = a[i + 1], a[i]
+        m += 1
+    print(f'Количество операций внутренних: {counter}')
+    return a
+
+
+n = int(input('Введите размер массива: '))
+a = init_array(n)
+
+# a = [0, 9, 1, 2, 3, 4, 5, 6, 7, 8]
+
+b = a.copy()
+print(f'Рандомный массив размера {n}:')
+print(a)
+
+a = buble_sort(a)
+print('Отсортированный массив (пузырьком):')
+print(a)
+
+b = buble_sort_opt(b)
+print('Отсортированный массив (пузырьком оптимизированным):')
+print(b)
